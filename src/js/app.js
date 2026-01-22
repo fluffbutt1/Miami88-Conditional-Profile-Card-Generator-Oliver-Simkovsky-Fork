d5b027c26ce3144e98648960045b770975c6dbbf
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2026-01-22 00:03:41 +0000

    finalized project, made sure whole page handles user input instead of hard coded variables

--- src/js/app.js
+++ src/js/app.js
@@ -36,7 +36,7 @@ function render(variables = {}) {
           <h1>${variables.name} ${variables.lastName}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city} ${variables.country}</h3>
-          <ul class="position-right">
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="${variables.github}"><i class="fab fa-github"></i></a></li>
             <li><a href="${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
